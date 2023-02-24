0dcd24b465fcafafd6e2305a297fc72bce66d578
Karina Fernandez <karina.eliz.fernandez@gmail.com>
2023-02-24 11:49:19 -0300

    archivos de pagina mecanica

---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+
+# mecanica_sergioariel
+https://karinaeliz.github.io/mecanica_sergioariel/
+<<<<<<< HEAD
+mecanica.png
+=======
+
+![mecanica](https://user-images.githubusercontent.com/110490197/209127187-8dd8aab1-ce9c-4ef8-ba18-9be1d2e9168e.png)
+>>>>>>> 39ab0fc0d7c727bcc39e205733ed484279b0743d
---
+++ toggleBtn.js
@@ -0,0 +1,11 @@
+const toggleBTn = document.querySelector('.toggle_btn')
+const toggleBTnIcon = document.querySelector('.toggle_btn i')
+const dropDownMenu = document.querySelector('.dropdown_menu')
+
+toggleBTn.onclick = function (){
+    dropDownMenu.classList.toggle('open')
+    const isOpen= dropDownMenu.classList.contains('open')
+    toggleBTnIcon.classList = isOpen
+    ? 'fa-solid fa-xmark'
+    : 'fa-solid fa-bars'
+}
